b07fbafb0afc53fab089c4111169cfc7b84231fe
sravan-iaisolution <sravan.kumar@iaisolution.com>
2025-09-23 22:34:28 +0530

    trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-
-
-const Home = async () => {
+import { getQueryClient, trpc } from '@/trpc/server';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { ClientGreeting } from './client';
+export default async function Home() {
+  const queryClient = getQueryClient();
+  void queryClient.fetchQuery(trpc.createAI.queryOptions({ text: 'prefetch' }));
+  // Do something with greeting on the server
   return (
-    <div>
-      hello world
-    </div>
-  )
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ClientGreeting />
+    </HydrationBoundary>
+  );
 }
-
-export default Home
