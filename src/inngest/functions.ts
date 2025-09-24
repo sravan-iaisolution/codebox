851ffef854cf367bba28620619868b0fce7b134c
sravan-iaisolution <sravan.kumar@iaisolution.com>
2025-09-24 16:22:50 +0530

    sandbox integration

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,13 +1,19 @@
 // src/inngest/functions.ts
 import { inngest } from "./client";
 import OpenAI from "openai";
+import {Sandbox} from "@e2b/code-interpreter"
+import { getSandbox } from "./utils";
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY! });
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world2" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event,step }) => {
+    const sandboxId=await step.run("get-sandbox-id",async()=>{
+      const sandBox=await Sandbox.create("sravan-test-2")
+      return sandBox.sandboxId
+    })
     const text = String(event.data?.value ?? "");
     const res = await openai.chat.completions.create({
       model: "gpt-4o-mini",
@@ -19,6 +25,12 @@ export const helloWorld = inngest.createFunction(
     });
 
     const output = res.choices?.[0]?.message?.content ?? "";
-    return { output};
+
+    const sandboxUrl=await step.run("get-sandbox-id",async ()=>{
+      const sandbox=await getSandbox(sandboxId)
+      const host= sandbox.getHost(3000)
+      return `https://${host}`
+    })
+    return { output,sandboxUrl};
   }
 );
